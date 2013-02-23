3761ae7543afe4a6613501be6fe3b6fdad4e3fe0
Ethan Furman <ethan@stoneleaf.us>
2013-02-22 17:30:37 -0800

    added custom Table to augment dbf.Table

--- __init__.py
+++ __init__.py
@@ -1,3 +1,20 @@
 from .generators import *
 from .iterators import *
 from .utils import *
+import dbf
+
+def Table(*args, **kwargs):
+    'default to Clipper, Char, Logical, etc'
+    data_types = {
+            'C' : dbf.Char,
+            'L' : dbf.Logical,
+            'D' : dbf.Date,
+            }
+    if 'default_data_types' in kwargs:
+        data_types.update(kwargs['default_data_types'])
+    kwargs['default_data_types'] = data_types
+    kwargs['dbf_type'] = 'clp'
+    if (len(args) > 1 or kwargs.get('field_specs') is not None) \
+    and ('codepage' not in kwargs):
+        kwargs['codepage'] = 'utf8'
+    return dbf.Table(*args, **kwargs)
