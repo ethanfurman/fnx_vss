38ea54fb40959114c67bc46a0d8742bdb31ae671
Ethan Furman <ethan@stoneleaf.us>
2013-03-27 14:01:34 -0700

    empty field names now get fld%02d % fieldnum names (which also puts mask in the right place; duplicate table name keys are now removed (table number keys are still present); DATACACHE is now keyed by table, simple, section

--- BBxXlate/fisData.py
+++ BBxXlate/fisData.py
@@ -1,9 +1,12 @@
 #!/usr/local/bin/python
 import sys, getpass, shlex, subprocess, re, os
-from fenx.BBxXlate.bbxfile import BBxFile
+from bbxfile import BBxFile
 
 FIS_SCHEMAS = "/FIS/Falcon_FIS_SCHEMA"
 FIS_DATA = "/FIS/data"
+
+# enable for text file output to compare against original output
+textfiles = False
 
 def sizefrom(mask):
     if not(mask): return ""
@@ -12,54 +15,94 @@ def sizefrom(mask):
     if "." in mask: postdec = len(mask.split(".")[-1])
     return "(%s,%s)" % (fieldlen,postdec)
 
+def slicendice(line, *nums):
+    results = []
+    start = None
+    nums += (None, )
+    for num in nums:
+        results.append(line[start:num].strip())
+        start = num
+    return tuple(results)
 
 def parse_FIS_Schema(source):
+    if textfiles:
+        file_fields = open('fields.txt.latest', 'w')
+        file_iolist = open('iolist.txt.latest', 'w')
+        file_tables = open('tables.txt.latest', 'w')
+    iolist = None    
     contents = open(source).readlines()
     FIS_TABLES = {}
-    for line in [ii for ii in contents if ii.strip()]:
+    for line in contents:
+        line = line.rstrip()
+        if not line:
+            continue
         if line.startswith("FC"):
+            if textfiles and iolist is not None:
+               file_iolist.write(str(iolist) + '\n')            
             name = line[2:9].strip()
             parts = line[9:].split(" ( ")
             desc = parts[0].strip()
             filenum = int(parts[1].split()[0])
-            fields = FIS_TABLES.setdefault(name,{'name':name,'desc':desc,'filenum':filenum,'fields':[],'iolist':[]})['fields']
-            iolist = FIS_TABLES[name]['iolist']
-            FIS_TABLES[filenum] = FIS_TABLES[name]
-            #print name,filenum,'  ==>'
-        elif line.startswith("          "):
-            continue
-        else:   # should start with a field number...
-            #print line
-            if len(line)>56:
-                fieldvar,fieldmask = line[56:].split()[0].strip(),line[56:].split()[-1]
-                if fieldvar == fieldmask: fieldmask = ""
-                fieldnum,fielddesc,fieldsize = line[:10].strip(),line[10:50].strip(),line[50:56].strip()
+            fields = FIS_TABLES.setdefault(filenum, {'name':name, 'desc':desc, 'filenum':filenum, 'fields':[], 'iolist':[]})['fields']
+            if name in FIS_TABLES:
+                del FIS_TABLES[name]    # only allow names if there aren't any duplicates
             else:
-                fieldnum,fielddesc,fieldsize,fieldvar = line[:10].strip(),line[10:50].strip(),line[50:56].strip(),"None"
+                FIS_TABLES[name] = FIS_TABLES[filenum]
+            iolist = FIS_TABLES[filenum]['iolist']
+        else:   # should start with a field number...
+            fieldnum, fielddesc, fieldsize, rest = slicendice(line, 10, 50, 56)
+            rest = rest.split()
+            if not rest:
+                fieldmask, fieldvar = '', 'None'
+                if fielddesc.strip('()').lower() != 'open':
+                    fieldvar = 'Fld%02d' % int(fieldnum)
+            else:
+                if '#' in rest[-1]:
+                    fieldmask = rest.pop()
+                    if not rest:
+                        rest.append('Fld%02d' % int(fieldnum))
+                else:
+                    fieldmask = ''
+                if len(rest) == 2:
+                    fieldvar, maybe = rest
+                    if '(' in maybe:
+                        fieldvar = maybe
+                else:
+                    fieldvar = rest[0]
             if "(" in fieldvar and not fieldvar.endswith(")"):
-                fieldvar+=")"
-            #print " --> ",fieldnum,fielddesc,fieldsize,fieldvar,fieldmask
-            if not(fieldsize): fieldsize = "0"
-            fields.append(["f%s_%s" % (filenum,fieldnum),fielddesc,int(fieldsize),fieldvar,sizefrom(fieldmask)])
-            if "$" in fieldvar: basevar = fieldvar.split("(")[0]
-            else: basevar = fieldvar
-            if not basevar in iolist: iolist.append(basevar)
+                fieldvar+=")"                    
+            fieldsize = int(fieldsize) if fieldsize else 0
+            fields.append(["f%s_%s" % (filenum,fieldnum), fielddesc, fieldsize, fieldvar, sizefrom(fieldmask)])
+            if textfiles:
+                file_fields.write(str(fields[-1]) + '\n')            
+            if "$" in fieldvar:
+                basevar = fieldvar.split("(")[0]
+            else:
+                basevar = fieldvar
+            if not basevar in iolist:
+                iolist.append(basevar)
+    if text_files:
+        file_iolist.write(str(iolist) + '\n')
+        for key, value in sorted(FIS_TABLES.items(), key=lambda kv: kv[1]['filenum']):
+            file_tables.write("%-10s %5s - %-10s  %s\n" % (key, value['filenum'], value['name'], value['desc']))            
     return FIS_TABLES
 
 
 DATACACHE = {}
 
-def fisData (table,simple=None,section=None):
-    if table in DATACACHE:
-        return DATACACHE[table]
+def fisData (table, simple=None, section=None):
+    key = table, simple, section
+    if key in DATACACHE:
+        return DATACACHE[key]
     datamap = tables[table]['iolist']
     tablename = tables[table]['name']
     datafile = os.sep.join([FIS_DATA,"O"+tablename[:4]])
-    DATACACHE[table] = BBxFile(datafile,datamap,simple=simple,section=section)
-    return DATACACHE[table]
+    table = DATACACHE[key] = BBxFile(datafile, datamap, simple=simple, section=section)
+    return table
 
+parse_FIS_Schema(FIS_SCHEMAS)
 
-tables = parse_FIS_Schema(FIS_SCHEMAS)
+#tables = parse_FIS_Schema(FIS_SCHEMAS)
 
 #tables['NVTY1']['fields'][77]
 
