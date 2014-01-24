d4f4b7f52070b4923c3db6ac214a5f0c916777d0
Ethan Furman <ethan@stoneleaf.us>
2014-01-24 12:09:43 -0800

    moved ACHPayment from utils to finance; mail now accepts a message as a string

--- utils.py
+++ utils.py
@@ -14,6 +14,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.Encoders import encode_base64
+from email import email
 from VSS import dbf
 from VSS.dbf import DateTime, Date, Time, Integer, String
 from enum import Enum, IntEnum
@@ -2921,9 +2922,9 @@ def Time_as_float(t):
 
 def mail(server, port, message):
     """sends email.message to server:port
-
-    receiver is a list of addresses
     """
+    if isinstance(message, String):
+        message = email.message_from_string(message)
     receiver = message.get_all('To', []) + message.get_all('Cc', []) + message.get_all('Bcc', [])
     sender = message['From']
     smtp = smtplib.SMTP(server, port)
@@ -3023,54 +3024,6 @@ class suppress(object):
         return etype is None or issubclass(etype, self.exceptions)
 
 
-class ACHPayment(object):
-    """A single payment from company to a vendor."""
-
-    _strip = staticmethod(translator(delete=' -'))
-
-    def __init__(self,
-            description, sec_code, 
-            vendor_name, vendor_inv_num, vendor_rtng, vendor_acct,
-            transaction_code, vendor_acct_type, amount):
-        """
-        description:  10 chars
-        sec_code: CCD or CTX
-        vendor_name: 22 chars
-        vendor_inv_num: 15 chars
-        vendor_rtng: 9 chars
-        vendor_acct: 17 chars
-        transaction_code: ACH_ETC code
-        vendor_acct_type: 'domestic' or 'foreign'
-        amount: 10 digits (pennies)
-        """
-        self.description = description.upper()
-        self.sec_code = sec_code.upper()
-        self.vendor_name = vendor_name.upper()
-        self.vendor_inv_num = str(vendor_inv_num).upper()
-        self.vendor_rtng = self._strip(vendor_rtng)
-        self.vendor_acct = self._strip(vendor_acct)
-        self.transaction_code = transaction_code
-        self.vendor_acct_type = vendor_acct_type
-        self.amount = amount
-        self.validate_routing(self.vendor_rtng)
-
-    def __repr__(self):
-        return 'ACHPayment(%s)' % (', '.join([
-            repr(v) for v in (
-                self.description, self.sec_code, self.vendor_name, self.vendor_inv_num, self.vendor_rtng, self.vendor_acct,
-                self.transaction_code, self.vendor_acct_type, self.amount,
-                )]))
-
-    @staticmethod
-    def validate_routing(rtng):
-        total = 0
-        for digit, weight in zip(rtng, (3, 7, 1, 3, 7, 1, 3, 7)):
-            total += int(digit) * weight
-        chk_digit = (10 - total % 10) % 10
-        if chk_digit != int(rtng[-1]):
-            raise ValueError('Routing number %s fails check digit calculation' % rtng)
-
-
 class ProgressBar(object):
     def __init__(yo, finalcount, block_char='.', message=None):
         yo.current_count = 0
@@ -3089,9 +3042,7 @@ class ProgressBar(object):
         yo.current_count = count
         count = min(count, yo.finalcount)
         if yo.finalcount:
-            percentcomplete = int(round(100.0*count/yo.finalcount))
-            #if percentcomplete < 1:
-            #    percentcomplete = 1
+            percentcomplete = int(floor(100.0*count/yo.finalcount))
         else:
             percentcomplete = 100
         blockcount = int(percentcomplete//2)
--- wellsfargo.py
+++ wellsfargo.py
@@ -4,7 +4,8 @@ from itertools import groupby
 from VSS import Table, Month, Weekday, days_per_month, AutoEnum, IntEnum, OrderedDict
 from VSS.BBxXlate.bbxfile import BBxFile
 from VSS.path import Path
-from VSS.utils import one_day, bb_text_to_date, text_to_date, text_to_time, xrange, Date, Time, OrderedDict, ACHPayment
+from VSS.utils import one_day, bb_text_to_date, text_to_date, text_to_time, xrange, Date, Time, OrderedDict
+from VSS.finance import ACHPayment
 
 try:
     next
