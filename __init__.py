e29bbe92dba872a58621dd0717a6f5564867e533
Ethan Furman <ethan@stoneleaf.us>
2014-01-24 17:09:29 -0800

    added finance to __init__

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
 import dbf
 import enum
 import path
+import finance
 
 import sys
 sys.modules['VSS.dbf'] = dbf
 sys.modules['VSS.enum'] = enum
+sys.modules['VSS.finance'] = finance
 sys.modules['VSS.path'] = path
 
 from utils import *
