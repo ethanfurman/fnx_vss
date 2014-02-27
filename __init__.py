1c5c8aff94579ba81618e983caed860278aaf55a
Ethan Furman <ethan@stoneleaf.us>
2014-02-27 10:47:57 -0800

    fix imports

--- __init__.py
+++ __init__.py
@@ -1,12 +1,16 @@
+import address
 import dbf
 import enum
-import path
 import finance
+import path
+import time_machine
 
 import sys
+sys.modules['VSS.address'] = address
 sys.modules['VSS.dbf'] = dbf
 sys.modules['VSS.enum'] = enum
 sys.modules['VSS.finance'] = finance
 sys.modules['VSS.path'] = path
+sys.modules['VSS.time_machine'] = time_machine
 
 from utils import *
